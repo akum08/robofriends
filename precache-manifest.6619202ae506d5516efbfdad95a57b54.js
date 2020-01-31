c56ac28aa3f708a53b37c0128850c12110366e79
Amit Kumar228 <Amit_Kumar228@ad.infosys.com>
2020-01-31 21:56:21 +0530

    Updates

---
+++ precache-manifest.6619202ae506d5516efbfdad95a57b54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb8ded774592e1aa0715de745e5735b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a07abf2d26413080d0f7",
+    "url": "/robofriends/static/css/main.0ff520dc.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "a07abf2d26413080d0f7",
+    "url": "/robofriends/static/js/main.37de2038.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
